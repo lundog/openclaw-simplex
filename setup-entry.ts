588527c2936ca899cf21473dcf883db113d89f85
dangoldbj <dangoldbj23@gmail.com>
2026-06-03 23:36:09 +0200

    refactor setup entry

--- setup-entry.ts
+++ setup-entry.ts
@@ -1,4 +1,6 @@
 import { defineSetupPluginEntry } from "openclaw/plugin-sdk/channel-core";
 import { simplexPlugin } from "./src/channel/plugin.js";
 
-export default defineSetupPluginEntry(simplexPlugin);
+const setupEntry: ReturnType<typeof defineSetupPluginEntry> = defineSetupPluginEntry(simplexPlugin);
+
+export default setupEntry;
